85dd5f5d0309f6cf0267558ae664fc5069027541
greatraksin <greatraksin@icloud.com>
2023-09-30 15:54:14 +0300

    adding loops lesson 1

---
+++ 03_loops_3009/02_multi_condition.py
@@ -0,0 +1,6 @@
+letter = input('Введи букву "а": ')
+
+while letter != 'a' and letter != 'A':
+    letter = input('Введи букву "а": ')
+
+print('спасибо')
---
+++ 03_loops_3009/07_multiple_table.py
@@ -0,0 +1,6 @@
+n = int(input('Введи число: '))
+n1 = int(input('Введи множитель: '))
+
+for number in range(1, n1 + 1):
+    print(n, '*', number, '=', n * number)
+
